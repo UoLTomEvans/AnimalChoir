d5b19188ac137667ef2bbe832250a3281d712a0f
7R4C3 <tommarkevans@gmail.com>
2022-02-09 23:11:31 +0000

    finished gui

--- src/home/Main.java
+++ src/home/Main.java
@@ -4,18 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Objects;
 
 public class Main extends Application {
 
+    double x = 0;
+    double y = 0;
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void start(Stage stage) throws Exception{
 
+        Image icon = new Image("home/icons/icon.png");
+
+        URL url = getClass().getClassLoader().getResource("home/fxml/Main.fxml");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(url));
+        Scene sc = new Scene(root);
+        stage.setTitle("Animal Choir");
+        stage.getIcons().add(icon);
+        stage.setScene(sc);
+        stage.show();
+
+        // set min size
+        stage.setMinWidth(stage.getWidth());
+        stage.setMinHeight(stage.getHeight());
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/home/controllers/Controller.java
+++ src/home/controllers/Controller.java
@@ -1,4 +1,60 @@
 package home.controllers;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private AnchorPane homePage;
+
+    @FXML
+    private AnchorPane textInput;
+
+    @FXML
+    private AnchorPane MIDIFileUpload;
+
+    // scene switch handling
+    @FXML
+    public void switchToHome(ActionEvent event) throws IOException {
+        homePage.setVisible(true);
+        textInput.setVisible(false);
+        MIDIFileUpload.setVisible(false);
+    }
+
+    @FXML
+    public void switchToTextInput(ActionEvent event) throws IOException {
+        homePage.setVisible(false);
+        textInput.setVisible(true);
+        MIDIFileUpload.setVisible(false);
+    }
+
+    @FXML
+    public void switchToMIDIUpload(ActionEvent event) throws IOException {
+        homePage.setVisible(false);
+        textInput.setVisible(false);
+        MIDIFileUpload.setVisible(true);
+    }
+
+    // hyperlink
+    @FXML
+    public void openLink(ActionEvent event) throws URISyntaxException, IOException {
+        Desktop.getDesktop().browse(new URI("https://www.dogsandbirds.co.uk/how-it-works/"));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        homePage.setVisible(true);
+        textInput.setVisible(false);
+        MIDIFileUpload.setVisible(false);
+    }
 }
